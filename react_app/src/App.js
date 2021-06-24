032aee87dbbdfcd13d067a78901b6344d25ee1af
makskorp <korpmaks@yandex.ru>
2021-06-24 15:06:58 +0400

    first build

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "@rjsf/bootstrap-4"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button, ButtonGroup } from "react-bootstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const schema = {
+  "title": "Настройки",
+  "type": "object",
+  "submit": "Запустить",
+  "required": [
+    "start"
+  ],
+  "properties": {
+    "swap": {
+      "type": "boolean",
+      "title": "swap",
+      "default": false
+    },
+    "endian": {
+      "type": "string",
+      "title": "endian",
+      "enum": ["little", "big"],
+      "default": "little"
+    },
+    "period": {
+      "type": "number",
+      "title": "период обновлений, сек",
+      "enum": [1, 5, 10, 15, 30, 60],
+      "default": 10,
+    },
+    "port": {
+      "type": "number",
+      "title": "порт (0..65533)",
+      "minimum": 0,
+      "maximum": 65535,
+      "default": 502
+    },
+    "start": {
+      "type": "number",
+      "title": "стартовый регистр (0..65533)",
+      "minimum": 0,
+      "maximum": 65533,
+      "default": 30001
+    }
+  }
+}
+
+const uiSchema = {
+  "start": {
+    "ui:autofocus": true
+  }
+}
+
+const App = () => {
+  return <Container fluid>
+      <Row>
+        <Col xs={5}>
+          <h5>Значения</h5>
+        </Col>
+        <Col xs={5}>
+          <h5>Журнал</h5>
+        </Col>
+        <Col xs={2}>
+          <Form schema={schema} uiSchema={uiSchema}>
+            <ButtonGroup>
+              <Button variant="primary" type="submit">Save</Button>
+              <Button variant="success" type="submit">On</Button>
+              <Button variant="danger" type="submit">Off</Button>
+            </ButtonGroup>
+          </Form>
+        </Col>
+      </Row>
+  </Container>
+    
 }
 
 export default App;
